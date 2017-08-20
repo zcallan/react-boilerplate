2552f75d3d5c355c8f73155dee02948984cf5325
zcallan <kaln.delbridge@hotmail.com>
2017-08-20 13:28:27 +1000

    added react, created babelrc file

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,1 +1,6 @@
-console.log( 'Hello' );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from 'views';
+
+
+ReactDOM.render( <App />, document.getElementById( 'root' ));
---
+++ src/views/app/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>React Boilerplate</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
